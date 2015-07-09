aed0ef3eabd725b46f3df65fc21c3acf2fb1ab53
Alberto Mijares <alberto.mijares@edorasware.com>
2015-07-09 23:54:25 +0200

    add parameter support for html-renderer

--- bin/slr.js
+++ bin/slr.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
-var assert = require('../src/utils').assert;
+var _ = require('lodash'), assert = require('../src/utils').assert,
+    expandFilenames = require('../src/utils').expandFilenames, Report = require('../src/spec-layout-reporter').Report;
 
 assert(process.argv.length >= 3, [
     'usage: slr <file matcher>',
@@ -11,13 +12,27 @@ assert(process.argv.length >= 3, [
     '    slr src/lib/**/**/*.spec.js'
 ]);
 
-var expandFilenames = require('../src/utils').expandFilenames, Report = require('../src/spec-layout-reporter').Report;
+var params = process.argv.slice(2), options = processParams(params), report = new Report();
 
-var report = new Report();
-
-expandFilenames(process.argv.slice(2))
+expandFilenames(params)
     .forEach(function (filename) {
         report.add(filename);
     });
 
-report.render();
+report.render(options.renderer);
+
+function processParams(params) {
+    return _.extend({}, processOption(params, '--html'));
+
+    function processOption(params, optionParam) {
+        var option = {}, optionIndex = params.indexOf(optionParam);
+
+        if (optionIndex >= 0) {
+            params.splice(optionIndex, 1);
+
+            option.renderer = require('../src/spec-layout-reporter/html-renderer');
+        }
+
+        return option
+    }
+}
