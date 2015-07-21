e7ef36ac8e7cbbb06f7874f60a45f21d7e49526d
Alberto Mijares <alberto.mijares@edorasware.com>
2015-07-21 17:24:45 +0200

    remove native dagre implementation and use dot format

--- src/ng-dependency-analyser/rendering/html-renderer.js
+++ src/ng-dependency-analyser/rendering/html-renderer.js
@@ -6,8 +6,10 @@ dot.templateSettings.strip = false;
 module.exports = function (report, outputFilename) {
     assert(outputFilename, 'Error: no output file name has been provided to the HTML renderer.');
 
-    var templateFile = fs.readFileSync(path.join(__dirname, 'template.html')),
-        htmlTemplate = dot.template(templateFile);
+    var htmlTemplateFile = fs.readFileSync(path.join(__dirname, 'template.html')),
+        htmlTemplate = dot.template(htmlTemplateFile),
+        dotTemplateFile = fs.readFileSync(path.join(__dirname, 'template.dot')),
+        dotTemplate = dot.template(dotTemplateFile);
 
-    writeWithPath(outputFilename, htmlTemplate(report));
+    writeWithPath(outputFilename, htmlTemplate({report: report, dot: dotTemplate(report)}));
 };
