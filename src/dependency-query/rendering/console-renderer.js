f5f1219624665124cce177a14c22c4c59d2450a2
Alberto Mijares <alberto.mijares@edorasware.com>
2015-08-28 18:22:41 +0200

    (feat): sort depq table by file name

--- src/dependency-query/rendering/console-renderer.js
+++ src/dependency-query/rendering/console-renderer.js
@@ -3,14 +3,20 @@ var AsciiTable = require('ascii-table');
 module.exports = function consoleRenderer(report) {
     var lastFileName, table = new AsciiTable().setHeading('File', 'Dependency', 'Version');
 
-    report.getDependencies().forEach(function (dependency) {
-        var currentFileName = dependency.getFileName(),
-            fileName = !lastFileName || lastFileName !== currentFileName ? currentFileName : '';
+    report.getDependencies()
+        .sort(byFileName)
+        .forEach(function (dependency) {
+            var currentFileName = dependency.getFileName(),
+                fileName = !lastFileName || lastFileName !== currentFileName ? currentFileName : '';
 
-        table.addRow(fileName, dependency.getName(), dependency.getVersion());
+            table.addRow(fileName, dependency.getName(), dependency.getVersion());
 
-        lastFileName = currentFileName;
-    });
+            lastFileName = currentFileName;
+        });
 
     console.log(table.toString());
 };
+
+function byFileName(dependency1, dependency2) {
+    return dependency1.getFileName() < dependency2.getFileName() ? -1 : 1;
+}
